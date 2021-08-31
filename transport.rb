3bc3778a1e6ae82bc80001de537661ae13730180
ryasenchuk <sergeyryasenchuk@gmail.com>
2021-08-31 21:38:10 +0300

    add new method and optimization deliveryservice

--- transport.rb
+++ transport.rb
@@ -1,12 +1,13 @@
 class Transport
-
+  attr_accessor :max_weight
+  include Comparable
   def initialize(max_weight, speed, available)
     @max_weight = max_weight
     @speed = speed
     @available = available
   end
 
-  attr_reader :available, :speed, :max_weight
+  attr_reader :available, :speed
 
   def max_weight_value
     max_weight if max_weight > 0
@@ -17,11 +18,15 @@ class Transport
   end
 
   def <=>(other)
-    speed_with_kg <=> other.speed_with_kg
+    (comparison_max_w == other.comparison_max_w) ? (comparison_max_d <=> other.comparison_max_d) : (comparison_max_w <=> other.comparison_max_w)
   end
 
-  def speed_with_kg
-    @speed.to_f / @max_weight
+  def comparison_max_w
+    @max_weight
+  end
+
+  def comparison_max_d
+    @max_distance || Float::INFINITY
   end
 
 end
