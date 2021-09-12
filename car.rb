e0d594bbc933eca7351aea318c499b084fce1ab7
ryasenchuk <sergeyryasenchuk@gmail.com>
2021-09-12 22:22:06 +0300

    change transport.rb add new methods

--- car.rb
+++ car.rb
@@ -6,7 +6,7 @@ class Car < Transport
   attr_accessor :registration_number
 
   def initialize
-    super( Constants::CAR[:max_weight], Constants::CAR[:speed],true )
+    super( Constants::CAR[:max_weight], Constants::CAR[:speed],true,Constants::CAR[:LOCATION],rand(100), 20 )
     @registration_number = rand(5000)
   end
 
--- constants.rb
+++ constants.rb
@@ -1,6 +1,7 @@
 module Constants
 
-  BIKE = { speed: 10, max_weight: 10, max_distance: 30, count: 2 }.freeze
-  CAR = { speed: 50, max_weight: 100, count: 2 }.freeze
+  BIKE = { speed: 10, max_weight: 10, max_distance: 30, count: 5, LOCATION: "on_garage" }.freeze
+  CAR = { speed: 50, max_weight: 100, count: 5, LOCATION: "on_garage" }.freeze
+  LOCATION = { on_route: "on_route", on_garage: "on_garage" }
 
 end
